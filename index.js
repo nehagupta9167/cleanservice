b8c70666bb8d6cf770a14b3dac980a30a779ff4a
nehagupta9167 <49072103+nehagupta9167@users.noreply.github.com>
2019-06-16 23:29:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,30 @@ app.post('/', function (req, res) {
   const agent = new WebhookClient({ request: req, response: res })
 
   
+  
+  app.post('/', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  if (req.body.Body == 'hello') {
+    twiml.message('Hi!');
+  } else if (req.body.Body == 'bye') {
+    twiml.message('Goodbye');
+  } else {
+    twiml.message(
+      'No Body param match, Twilio sends this in the request to your server.'
+    );
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log('Express server listening on port 1337');
+});
+  
+  
+  
   function bookservice (agent) {
 //   var givendate = req.body.queryResult.parameters['date']
 //   var giventime = req.body.queryResult.parameters['time']
