a4b40451af78357c23bf0295bf284b0dc210f703
nehagupta9167 <49072103+nehagupta9167@users.noreply.github.com>
2019-06-17 21:30:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,28 +36,39 @@ app.use(bodyParser.json());
 app.post('/', function (req, res) {
   const agent = new WebhookClient({ request: req, response: res })
 
-  
-  
- // app.post('/', (req, res) => {
+  app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
-  if (req.body.Body == 'hello') {
-    twiml.message('Hi!');
-  } else if (req.body.Body == 'bye') {
-    twiml.message('Goodbye');
-  } else {
-    twiml.message(
-      'No Body param match, Twilio sends this in the request to your server.'
-    );
-  }
+  twiml.message('The Robots are coming! Head for the hills!');
 
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
+  res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
-//});
+});
 
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
 });
+  
+ // app.post('/', (req, res) => {
+ // const twiml = new MessagingResponse();
+
+//   if (req.body.Body == 'hello') {
+//     twiml.message('Hi!');
+//   } else if (req.body.Body == 'bye') {
+//     twiml.message('Goodbye');
+//   } else {
+//     twiml.message(
+//       'No Body param match, Twilio sends this in the request to your server.'
+//     );
+//   }
+
+ // res.writeHead(200, { 'Content-Type': 'text/xml' });
+ // res.end(twiml.toString());
+//});
+
+//http.createServer(app).listen(1337, () => {
+//  console.log('Express server listening on port 1337');
+//});
   
   
   
